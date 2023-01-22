7fb2775d8ee6ca8a77474ae8a9189b3f04899a68
WilsonMungai <wilsonmuguthi@gmail.com>
2023-01-21 21:07:13 -0500

    starting up

---
+++ iosSlotMachineGame/iosSlotMachineGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosSlotMachineGameApp.swift
+//  iosSlotMachineGame
+//
+//  Created by Wilson Mungai on 2023-01-21.
+//
+
+import SwiftUI
+
+@main
+struct iosSlotMachineGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
