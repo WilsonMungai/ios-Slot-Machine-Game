e65626da6d6586709f01358fd7c99832e31dd714
WilsonMungai <wilsonmuguthi@gmail.com>
2023-01-22 20:29:55 -0500

    added program headers

--- iosSlotMachineGame/Managers/Modifiers/Modifiers.swift
+++ iosSlotMachineGame/Managers/Modifiers/Modifiers.swift
@@ -1,9 +1,21 @@
-//
-//  Modifiers.swift
-//  iosSlotMachineGame
-//
-//  Created by Wilson Mungai on 2023-01-21.
-//
+/*
+ 
+MAPD 724 Slot Machine
+ 
+Group 10 Members
+ 
+Nyarko, Betrand / Student Number: 301293794
+Muguthi, Wilson Mungai / Student Number: 301287641
+Du, Pengfei / Student Number: 301276081
+ 
+This is a simple slot machine game that has three reel images, a spin button that changes the images in the reels. The user can see the amount of coins they have in the player coins label, and also see the jackpot amount in  the jackpot label. There are different bet amounts which the user can select. The player has the permission to reset the game which will reset everything in the game. There is also an option to quit the game.
+ 
+ Version 1
+ 
+ Last modified: 22/January/2023
+ 
+ */
+
 
 import SwiftUI
 
@@ -28,7 +40,7 @@ struct playerScoreContainerModifier: ViewModifier {
         content .background(Capsule() .foregroundColor(Color("transparent")))
             .padding(.vertical, 4)
             .padding(.horizontal, 16)
-            .frame(minWidth: 128)
+            .frame(minWidth: 200)
     }
 }
 
--- iosSlotMachineGame/Views/ReelView.swift
+++ iosSlotMachineGame/Views/ReelView.swift
@@ -1,10 +1,20 @@
-//
-//  ReelView.swift
-//  iosSlotMachineGame
-//
-//  Created by Wilson Mungai on 2023-01-21.
-//
-
+/*
+ 
+MAPD 724 Slot Machine
+ 
+Group 10 Members
+ 
+Nyarko, Betrand / Student Number: 301293794
+Muguthi, Wilson Mungai / Student Number: 301287641
+Du, Pengfei / Student Number: 301276081
+ 
+This is a simple slot machine game that has three reel images, a spin button that changes the images in the reels. The user can see the amount of coins they have in the player coins label, and also see the jackpot amount in  the jackpot label. There are different bet amounts which the user can select. The player has the permission to reset the game which will reset everything in the game. There is also an option to quit the game.
+ 
+ Version 1
+ 
+ Last modified: 22/January/2023
+ 
+ */
 import SwiftUI
 
 struct ReelView: View {
